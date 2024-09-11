55e0469a2a29bd0981ac9fe01a9964add2dc7fb3
mac <mac@macs-MBP.lan>
2024-09-11 20:50:22 +0530

    designed appointment Ui complted

--- src/Container/AppointmentCard/index.js
+++ src/Container/AppointmentCard/index.js
@@ -7,57 +7,67 @@ import LCSWImage from '../../Assets/Images/LCSW.png';
 const AppointmentCard = ({ appointment }) => {
     return (
         <View style={styles.cardContainer}>
-            {/* <Text style={styles.time}>{appointment.time}</Text> */}
+            <Text style={styles.time}>{appointment.time}</Text>
             <View style={styles.detailsContainer}>
-                <View style={{ flexDirection: 'row'}}>
-                <Image source={LCSWImage} style={styles.careTeamImage} />
-                <View style={styles.rowStyle}>
-                    <Text style={styles.name}>{appointment.name}</Text>
-                    <Text style={styles.type}>{appointment.type}</Text>
-                    <TouchableOpacity
-                        style={
-                            appointment.status === 'Pending' ? styles.pendingButton :
-                                appointment.status === 'Upcoming' ? styles.upcomingButton :
-                                    appointment.status === 'Completed' ? styles.completedButton :
-                                        appointment.status === 'Cancelled' ? styles.cancelledButton :
-                                            styles.defaultButton
-                        }
-                    >
-                        <Text
+                <View style={{ flexDirection: 'row' }}>
+                    <Image source={LCSWImage} style={styles.careTeamImage} />
+                    <View style={styles.rowStyle}>
+                        <Text style={styles.name}>{appointment.name}</Text>
+                        <Text style={styles.type}>{appointment.type}</Text>
+                        <TouchableOpacity
                             style={
-                                appointment.status === 'Pending' ? styles.pendingButtonText :
-                                    appointment.status === 'Upcoming' ? styles.upcomingButtonText :
-                                        appointment.status === 'Completed' ? styles.completedButtonText :
-                                            appointment.status === 'Cancelled' ? styles.cancelledButtonText :
-                                                styles.buttonText
+                                appointment.status === 'Pending' ? styles.pendingButton :
+                                    appointment.status === 'Upcoming' ? styles.upcomingButton :
+                                        appointment.status === 'Completed' ? styles.completedButton :
+                                            appointment.status === 'Cancelled' ? styles.cancelledButton :
+                                                styles.defaultButton
                             }
-                        >{appointment.status}</Text>
-                    </TouchableOpacity>
+                        >
+                            <Text
+                                style={
+                                    appointment.status === 'Pending' ? styles.pendingButtonText :
+                                        appointment.status === 'Upcoming' ? styles.upcomingButtonText :
+                                            appointment.status === 'Completed' ? styles.completedButtonText :
+                                                appointment.status === 'Cancelled' ? styles.cancelledButtonText :
+                                                    styles.buttonText
+                                }
+                            >{appointment.status}</Text>
+                        </TouchableOpacity>
                     </View>
-                    </View>
-                    <TouchableOpacity
-                        style={
-                            appointment.buttonText === 'Join Session' ? styles.upcomingJoinButton :
-                                appointment.buttonText === 'View Details' ? styles.completedJoinButton :'null'
-                           
-                        }
-                    >
-                        <Text
+                </View>
+                <>
+                    {appointment.buttonText === 'Join Session' || appointment.buttonText === 'View Details' ?
+                        <TouchableOpacity
                             style={
-                                appointment.buttonText === 'Join Session' ? styles.upcomingJoinButtonText :
-                                    appointment.buttonText === 'View Details' ? styles.completedJoinButtonText :'null'
-                                       
+                                appointment.buttonText === 'Join Session' ? styles.upcomingJoinButton :
+                                    appointment.buttonText === 'View Details' ? styles.completedJoinButton : 'null'
+
                             }
-                        >{appointment.buttonText}</Text>
-                    </TouchableOpacity>
-                    <View style={styles.cancelbtnRow}>
-                    <TouchableOpacity style={appointment.decline === 'Decline' ? styles.declineButton :'null'}>                                                   
-                        <Text style={appointment.decline === 'Decline' ? styles.declineButtonText :'null'}>{appointment.decline}</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={appointment.accept === 'Accept' ? styles.acceptButton :'null'}>                                                   
-                        <Text style={appointment.accept === 'Accept' ? styles.acceptButtonText :'null'}>{appointment.accept}</Text>
-                </TouchableOpacity>
-                    </View>
+                        >
+                            <Text
+                                style={
+                                    appointment.buttonText === 'Join Session' ? styles.upcomingJoinButtonText :
+                                        appointment.buttonText === 'View Details' ? styles.completedJoinButtonText : 'null'
+
+                                }
+                            >{appointment.buttonText}</Text>
+                        </TouchableOpacity>
+                        : ''
+                    }
+                </>
+                <>
+                    {appointment.decline === 'Decline' ?
+                        <View style={styles.cancelbtnRow}>
+                            <TouchableOpacity style={appointment.decline === 'Decline' ? styles.declineButton : 'null'}>
+                                <Text style={appointment.decline === 'Decline' ? styles.declineButtonText : 'null'}>{appointment.decline}</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={appointment.accept === 'Accept' ? styles.acceptButton : 'null'}>
+                                <Text style={appointment.accept === 'Accept' ? styles.acceptButtonText : 'null'}>{appointment.accept}</Text>
+                            </TouchableOpacity>
+                        </View>
+                        : ''
+                    }
+                </>
             </View>
         </View>
     );
@@ -68,18 +78,22 @@ export default AppointmentCard;
 const styles = StyleSheet.create({
     cardContainer: {
         alignItems: 'center',
-        // backgroundColor:'yellow'
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
     },
     time: {
         transform: [{ rotate: '-90deg' }],
-        fontSize: responsiveFontSize(1.5),
-        fontWeight: '600',
-        color: Colors.grey,
+        fontSize: responsiveFontSize(1.4),
+        fontWeight: '500',
+        color: Colors.darkgrey,
         textAlign: 'center',
+        marginTop: responsiveHeight(3),
+
+
     },
     detailsContainer: {
         paddingHorizontal: responsiveWidth(4),
-        paddingVertical: responsiveHeight(3),
+        paddingVertical: responsiveHeight(2),
         backgroundColor: Colors.white,
         shadowColor: Platform.OS === 'ios' ? Colors.grey : Colors.black,
         shadowOffset: {
@@ -89,10 +103,11 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.4,
         shadowRadius: 4,
         elevation: 5,
-        width: '90%',
+        width: '85%',
         marginTop: responsiveHeight(2),
         borderRadius: 12,
-        alignSelf: 'center',
+        marginRight: responsiveWidth(13)
+
 
     },
     name: {
@@ -199,19 +214,19 @@ const styles = StyleSheet.create({
         paddingHorizontal: responsiveWidth(15),
         alignItems: 'center',
         justifyContent: 'center',
-        borderWidth:1.5,
+        borderWidth: 1.5,
         marginTop: responsiveHeight(2),
     },
 
     upcomingJoinButtonText: {
         color: Colors.white,
         fontSize: responsiveFontSize(1.8),
-        fontWeight:'600'
+        fontWeight: '600'
     },
     completedJoinButtonText: {
         color: Colors.black,
         fontSize: responsiveFontSize(1.8),
-        fontWeight:'600'
+        fontWeight: '600'
 
     },
 
@@ -239,5 +254,9 @@ const styles = StyleSheet.create({
         fontSize: responsiveFontSize(1.8),
         fontWeight: 'bold',
     },
-    cancelbtnRow:{ flexDirection: 'row',justifyContent:'space-between'}
+    cancelbtnRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: responsiveHeight(2)
+    }
 });
