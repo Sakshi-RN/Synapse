8d1e805b6bb5bdc7bacfd2c03984a54e303b367c
mac <mac@macs-MacBook-Pro.local>
2024-11-15 18:25:08 +0530

    login api

--- src/Screens/SignIn/index.js
+++ src/Screens/SignIn/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     View, Text, StyleSheet, ImageBackground,
-    TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, ScrollView
+    TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView, Platform, ScrollView, ActivityIndicator
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { Logo } from '../../Assets/svg';
@@ -10,12 +10,49 @@ import { responsiveFontSize, responsiveHeight, responsiveWidth } from 'react-nat
 import Colors from '../../Themes/Colors';
 import CustomTextInput from '../../Components/CustomTextInput';
 import CustomButton from '../../Components/CustomButton';
+import axios from 'axios';
+import Loader from '../../Components/Loader';
 
 const SignIn = () => {
     const navigation = useNavigation();
+    const [email, setEmail] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
-    const handleVerifyPress = () => {
-        navigation.navigate('OtpVerify');
+    const handleVerifyPress = async () => {
+        if (!email.trim()) {
+            setErrorMessage('Please enter email id');
+            return;
+        }
+
+        setIsLoading(true);
+
+        try {
+            const apiKey = 'e1693d9245c57be86afc22ad06eda84c9cdb74dae6d56a8a7f71a93facb1f42b';
+            const response = await axios.post(
+                'https://eb1.taramind.com/client/login',
+                { email },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-API-KEY': apiKey,
+                    },
+                }
+            );
+
+            setIsLoading(false);
+
+            if (response.data.message === "Email exists") {
+
+                navigation.navigate('BTabNavigation');
+            } else {
+                setErrorMessage('Email does not exist');
+            }
+        } catch (error) {
+            setIsLoading(false);
+            console.error(error);
+            setErrorMessage('An error occurred. Please try again.');
+        }
     };
 
     return (
@@ -28,23 +65,31 @@ const SignIn = () => {
                     <ImageBackground source={images.bgImg} style={styles.container}>
                         <Logo width={160} />
                     </ImageBackground>
-                        <View style={styles.formContainer}>
-                            <Text style={styles.title}>Enter your Email Id</Text>
-                            <Text style={styles.centerText}>
-                                Please confirm your Email Id and Enter
-                                the code sent to your Email Id.
-                            </Text>
-                            <CustomTextInput
-                                placeholder="Enter your Email Id"
-                                inputStyle={styles.centerStyle}
-                            />
-                            <CustomButton
-                                title='Verify'
-                                buttonStyle={styles.btnStyle}
-                                onPress={handleVerifyPress}
-                            />
-                        </View>
-                 
+                    <View style={styles.formContainer}>
+                        <Text style={styles.title}>Enter your Email Id</Text>
+                        <Text style={styles.centerText}>
+                            Please confirm your Email Id and Enter
+                            the code sent to your Email Id.
+                        </Text>
+                        <CustomTextInput
+                            placeholder="Enter your Email Id"
+                            value={email}
+                            onChangeText={setEmail}
+                            inputStyle={styles.centerStyle}
+                        />
+                        {errorMessage ? <Text style={styles.errorText}>{errorMessage}</Text> : null}
+                        {isLoading ? (
+                            <Loader />
+                        ) : null}
+                        <CustomButton
+                            title={'Verify'}
+                            buttonStyle={styles.btnStyle}
+                            onPress={handleVerifyPress}
+                            disabled={isLoading}
+                        >
+
+                        </CustomButton>
+                    </View>
                 </ScrollView>
             </TouchableWithoutFeedback>
         </KeyboardAvoidingView>
@@ -54,12 +99,10 @@ const SignIn = () => {
 const styles = StyleSheet.create({
     content: { flex: 1, backgroundColor: Colors.white },
     container: {
-        height:  Platform.OS === 'ios' ?responsiveHeight(50):responsiveHeight(54),
+        height: Platform.OS === 'ios' ? responsiveHeight(50) : responsiveHeight(54),
         alignItems: 'center',
-        paddingTop:Platform.OS === 'ios' ? responsiveHeight(8):responsiveHeight(5),
-
+        paddingTop: Platform.OS === 'ios' ? responsiveHeight(8) : responsiveHeight(5),
     },
-
     formContainer: {
         flex: 1,
     },
@@ -79,12 +122,20 @@ const styles = StyleSheet.create({
     },
     centerStyle: {
         marginHorizontal: responsiveWidth(5),
-        bottom:responsiveHeight(1)
+        bottom: responsiveHeight(1)
     },
     btnStyle: {
         alignSelf: 'center',
-        marginTop: responsiveHeight(4)
-    }
+        marginTop: responsiveHeight(4),
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    errorText: {
+        color: 'red',
+        marginTop: responsiveHeight(1),
+        marginLeft: responsiveWidth(6)
+    },
 });
 
 export default SignIn;
